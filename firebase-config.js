664d9580b6068906fec8983a28e733be9f111dcb
Shine Wanna <shinewanna97@gmail.com>
2021-04-21 12:45:51 +0630

    profolio readded

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDQ1Kj0M_6CMnt7PvG9dJdiGU0x6qqsc0g",
+    authDomain: "share-notification-abc3f.firebaseapp.com",
+    projectId: "share-notification-abc3f",
+    storageBucket: "share-notification-abc3f.appspot.com",
+    messagingSenderId: "690191939123",
+    appId: "1:690191939123:web:7f2f83a8082e64b63f5546",
+    measurementId: "G-XWL1BV5FGF"
+};
