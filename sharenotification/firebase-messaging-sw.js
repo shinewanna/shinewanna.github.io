f9fffc09ec5d2ebac8997751b6fa8fce3e06d4e4
Shine Wanna <shinewanna97@gmail.com>
2021-04-21 13:34:34 +0630

    profolio readded

--- sharenotification/firebase-messaging-sw.js
+++ sharenotification/firebase-messaging-sw.js
@@ -1,7 +1,15 @@
 importScripts("https://www.gstatic.com/firebasejs/7.23.0/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/7.23.0/firebase-messaging.js");
-import { firebaseConfig } from './firebase-config.js';
-firebase.initializeApp(firebaseConfig);
+
+firebase.initializeApp({
+    apiKey: "AIzaSyDQ1Kj0M_6CMnt7PvG9dJdiGU0x6qqsc0g",
+    authDomain: "share-notification-abc3f.firebaseapp.com",
+    projectId: "share-notification-abc3f",
+    storageBucket: "share-notification-abc3f.appspot.com",
+    messagingSenderId: "690191939123",
+    appId: "1:690191939123:web:7f2f83a8082e64b63f5546",
+    measurementId: "G-XWL1BV5FGF"
+});
 const messaging = firebase.messaging();
 messaging.setBackgroundMessageHandler(function (payload) {
     const promiseChain = clients
@@ -19,7 +27,7 @@ messaging.setBackgroundMessageHandler(function (payload) {
             const title = payload.notification.title;
             const options = {
                 body: payload.notification.score
-              };
+            };
             return registration.showNotification(title, options);
         });
     return promiseChain;
